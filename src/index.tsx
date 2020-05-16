a2197580e15f1ad52b8b60027583c7ed20837877
GBernardo10 <g_bernardo10@hotmail.com>
2020-05-16 01:44:39 -0300

    (src/index.tsx add type for parameters next - HandlerFunc)

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@
 
 import React from "https://dev.jspm.io/react";
 import ReactDOMServer from "https://dev.jspm.io/react-dom/server";
-import Home from "./components/home/index.tsx";
 
 import { Application } from "https://deno.land/x/abc/mod.ts";
 import { Context } from "https://deno.land/x/abc/context.ts";
+import { HandlerFunc } from "https://deno.land/x/abc/types.ts";
+
+import Home from "./components/home/index.tsx";
 
 const app = new Application();
 const port = 3000;
 
-app.use((next) => (c: Context) => {
+app.use((next: HandlerFunc) => (c: Context) => {
   let e = next(c);
   if (React.isValidElement(e)) {
     e = ReactDOMServer.renderToString(e);
